2e9e054ff79d7d5b6e3cd5d4d052c08b8c043777
Deepak Shah <shahkrdeepak@gmail.com>
2024-10-23 15:39:23 -0500

    Base map component

---
+++ playground/src/pages/Components/s-base-map.vue
@@ -0,0 +1,61 @@
+<template>
+    <component-doc title="SBaseMap">
+        <template #description>
+            SBaseMap offers a basemap component for the map, including options
+            such as CartoDB, OpenStreetMap, Google Satellite, Terrain, Dark
+            Gray, and OpenLayers.
+        </template>
+        <template #example>
+            <l-map
+                ref="lmap"
+                style="min-height: 500px"
+                :zoom="6"
+                :center="[47.21322, -1.559482]"
+                :options="{ zoomControl: false }"
+            >
+                <l-control position="topleft">
+                    <s-base-map :lmap="lmap" />
+                </l-control>
+                <LTileLayer
+                    url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+                    attribution='&amp;copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+                    layer-type="base"
+                    name="OpenStreetMap"
+                />
+            </l-map>
+        </template>
+        <template #prop>
+            <v-table>
+                <thead class="bg-floor">
+                    <tr>
+                        <th>Props</th>
+                        <th>Type</th>
+                        <th>Defaults</th>
+                        <th>Comment</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>lmap</td>
+                        <td>any</td>
+                        <td></td>
+                        <td>
+                            <i>lmap</i> props contains the reference to leaflet
+                            map.
+                        </td>
+                    </tr>
+                </tbody>
+            </v-table>
+        </template>
+    </component-doc>
+</template>
+
+<script setup lang="ts">
+import L from 'leaflet'
+import { LMap, LTileLayer, LControl } from '@vue-leaflet/vue-leaflet'
+
+import ComponentDoc from '~/components/layout/ComponentDoc.vue'
+const lmap = ref<any>(null)
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/runtime/components/SBaseMap.vue
@@ -0,0 +1,172 @@
+<template>
+    <v-expansion-panels v-model="panel">
+        <v-expansion-panel color="primary">
+            <v-expansion-panel-title
+                expand-icon="mdi-map"
+                collapse-icon="mdi-close"
+                v-slot="{ expanded }"
+                density=""
+            >
+                <b>{{ expanded ? 'BaseMap' : ' ' }}</b>
+            </v-expansion-panel-title>
+            <v-expansion-panel-text>
+                <v-card
+                    class="mx-auto"
+                    :width="smAndDown ? '300px' : '700px'"
+                    variant="flat"
+                >
+                    <v-row>
+                        <v-col
+                            v-for="(layer, index) in layers"
+                            :key="index"
+                            cols="4"
+                            class="pa-1"
+                        >
+                            <v-hover v-slot="{ isHovering, props }">
+                                <v-card
+                                    class="pa-0"
+                                    color="grey-lighten-4"
+                                    v-bind="props"
+                                    @click="changeBaseLayer(layer.title)"
+                                >
+                                    <v-img
+                                        :aspect-ratio="16 / 9"
+                                        :src="layer.image"
+                                        cover
+                                    >
+                                        <v-expand-transition>
+                                            <div
+                                                v-if="isHovering"
+                                                class="d-flex transition-fast-in-fast-out bg-primary-darken-1 v-card--reveal text-h7 align-center justify-center"
+                                                style="height: 100%"
+                                            >
+                                                {{ layer.title }}
+                                            </div>
+                                        </v-expand-transition>
+                                    </v-img>
+                                </v-card>
+                            </v-hover>
+                        </v-col>
+                    </v-row>
+                </v-card>
+            </v-expansion-panel-text>
+        </v-expansion-panel>
+    </v-expansion-panels>
+</template>
+
+<script setup lang="ts">
+import L from 'leaflet'
+
+import { useDisplay } from 'vuetify'
+const { smAndDown } = useDisplay()
+interface Props {
+    lmap: any
+}
+const props = withDefaults(defineProps<Props>(), {})
+const panel = ref(null)
+const selectedLayer = ref<string | null>(null)
+
+const layers = [
+    {
+        title: 'CartoDB',
+        image: '/basemaps/cartodb.png',
+        layer: L.tileLayer(
+            'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+            {
+                attribution: '&copy; CartoDB',
+                'layer-type': 'base',
+            }
+        ),
+    },
+    {
+        title: 'OpenStreetMap',
+        image: '/basemaps/openstreetmap.png',
+        layer: L.tileLayer(
+            'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            {
+                attribution: '&copy; OpenStreetMap contributors',
+            }
+        ),
+    },
+
+    {
+        title: 'Google Satellite',
+        image: '/basemaps/google.png',
+        layer: L.tileLayer(
+            'https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
+            {
+                maxZoom: 20,
+                subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+                attribution: 'Tiles Â© Map data Â©2019 Google',
+            }
+        ),
+    },
+    {
+        title: 'Terrain',
+        image: '/basemaps/terrain.png',
+        layer: L.tileLayer(
+            'https://{s}.tile.jawg.io/jawg-terrain/{z}/{x}/{y}{r}.png?access-token={accessToken}',
+            {
+                attribution:
+                    '<a href="https://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                minZoom: 0,
+                maxZoom: 22,
+                subdomains: 'abcd',
+                accessToken:
+                    'rU9sOZqw2vhWdd1iYYIFqXxstyXPNKIp9UKC1s8NQkl9epmf0YpFF8a2HX1sNMBM',
+                opacity: 1,
+            }
+        ),
+    },
+    {
+        title: 'Dark Gray',
+        image: '/basemaps/dark.png',
+        layer: L.tileLayer.wms(
+            'https://server.arcgisonline.com/arcgis/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}',
+            {
+                format: 'image/jpeg',
+                transparent: true,
+                attribution:
+                    'Tiles Â© <a href="https://services.arcgisonline.com/ArcGIS/' +
+                    'rest/services/Reference/Specialty/DeLorme_World_Base_Map/MapServer">ArcGIS</a>',
+                opacity: 1,
+            }
+        ),
+    },
+    {
+        title: 'Open Topo Layer',
+        image: '/basemaps/topo.png',
+        layer: L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+            maxZoom: 17,
+            attribution:
+                'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+        }),
+    },
+]
+
+const changeBaseLayer = (layerTitle) => {
+    if (!props.lmap) return
+
+    layers.forEach((layer) => {
+        if (props.lmap.leafletObject.hasLayer(layer.layer)) {
+            props.lmap.leafletObject.removeLayer(layer.layer)
+        }
+    })
+
+    const newLayer = layers.find((layer) => layer.title === layerTitle)
+    if (newLayer) {
+        props.lmap.leafletObject.addLayer(newLayer.layer)
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.v-card--reveal {
+    align-items: center;
+    bottom: 0;
+    justify-content: center;
+    opacity: 0.7;
+    position: absolute;
+    width: 100%;
+}
+</style>
